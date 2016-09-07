40d98af1895eb924562521e3784cd9a5f8891d68
Rikard Fröberg <rikard@morus.se>
2016-09-07 18:14:05 +0200

    More javadoc

--- src/org/addressbook/main/SimpleApplication.java
+++ src/org/addressbook/main/SimpleApplication.java
@@ -6,9 +6,28 @@ import org.addressbook.ui.cli.menu.MenuAction;
 import java.util.Iterator;
 import static org.addressbook.textutils.TextUtils.askFor;
 
+/**
+ * This class represents the application logic of the address book.
+ * It creates the menu for the application, and provides the logic
+ * for each menu item.
+ *
+ * The idea is to allow for the main method for the application 
+ * to be kept short and simple, by letting main create an instance
+ * of this class, and let the instance do the work of initiating
+ * the menu and starting it.
+ *
+ * You may use this class and complete the createMenu() method
+ * if you don't want to write an alternative class from scratch.
+ */
+
 public class SimpleApplication{
   private SimpleMutableList<Contact> list;
   private Menu menu = new Menu("Address book");
+  /**
+   * Constructs a new SimpleApplication
+   * with a fresh address book loaded from file
+   * if the file exists.
+   */
   public SimpleApplication(){
     list = new SimpleAddressBook();
     list.load();
@@ -47,6 +66,9 @@ public class SimpleApplication{
      */
   }
 
+  /**
+   * Starts this application - setting up and starting the menu system.
+   */
   public void start(){
     createMenu();
     menu.start();
--- src/org/addressbook/textutils/TextUtils.java
+++ src/org/addressbook/textutils/TextUtils.java
@@ -1,16 +1,24 @@
 package org.addressbook.textutils;
 
+/**
+ * A utility class with (so far) one static method for getting
+ * values from the user, using standard out and standard in for
+ * communication.
+ */
 public class TextUtils{
 
+  /* We don't want client code to instantiate this class */
+  private TextUtils(){}
+
   /**
-   * Asks the user a question and returns the result
-   * @param prompt The question to ask the user, e.g. "Name: "
-   * @return The user's reply as a reference to a String
-   *
+   * Asks the user a question and returns the result.
    * Note, this method doesn't check a user reply of NULL,
    * the empty String "". Nor does it trim the user's reply.
    *
    * These limitations are topics for the next assignment!
+   * @param prompt The question to ask the user, e.g. "Name: "
+   * @return The user's reply as a reference to a String
+   *
    */
   public static String askFor(String prompt){
     String result=null;
